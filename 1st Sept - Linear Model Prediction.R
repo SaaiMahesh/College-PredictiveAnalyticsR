b4a6bad322acd9bf98b1f9e2b84d4980d84328e9
SaaiMahesh <82525561+SaaiMahesh@users.noreply.github.com>
2021-09-01 18:35:01 +0530

    Update 1st Sept - Linear Model Prediction.R

--- 1st Sept - Linear Model Prediction.R
+++ 1st Sept - Linear Model Prediction.R
@@ -2,7 +2,7 @@ height <- c(121,164,148,186,178,156,179,163,152,131,138,166,189,123,178,161,149,
 weight <- c(53,71,56,79,68,55,67,62,51,48,53,68,83,54,74,58,52,78,67,52) #Dependent variable
 
 #Linear modelling using lm()
-model1 <-lm(weight~height) #Using ~ is the syntax. i.e dependent ~ independent.
+model1 <-lm(weight~height) 
 
 #Assumptions for linearity -
 #1.Outlier Test ()
@@ -26,11 +26,9 @@ cor.test(weight,height) #Since p is lesser than 0.05. Hence we accept alternativ
 #0.859 (cor) correlation means that there is 85.9% correlation between the variables.
 
 model1 #This shows us the linear model that we've obtained using lm() function.
-#Hence, weight = -8.295 + 0.4413 * height is the linear line equation.
+#Hence, weight = -8.295 + 0.4413 * height is the linear regression line equation.
 
-summary(model1)
-#Gives us R^2 and adjusted R^2 values.
-
+summary(model1) #Gives us R^2 and adjusted R^2 values.
 
 #The function lm() establishes a regression equation between the two variables height and weight. Wherein, height is independent variable and weight as dependent variable. The equation formed is weight = -8.295 + 0.4413 * height.
 #The summary function of model1 gives us high value of R^2 i.e 0.7389 showing that the model is good and can be used for prediction.
