99d70d2ae88d111f031f665496c693112ffc6d95
SaaiMahesh <82525561+SaaiMahesh@users.noreply.github.com>
2021-09-17 22:46:57 +0530

    Add files via upload

---
+++ Logistic Model Prediction 2.R
@@ -0,0 +1,24 @@
+#Building Linear Models
+data<-PimaIndiansDiabetes
+View(data)
+sum(is.na(data))
+sum(is.null(data))
+
+#NOTE: #glm(DV~.) #The dot gives us all the variables within the dataset
+
+#Model building
+model1<-glm(data$diabetes~.,data=data,family=binomial()) #Using binomial family to get output in 0s and 1s.
+summary(model1)
+#Discarding the variables of age,insulin,triceps since the p > 0.05 so it is insignificant to our model.
+model1<-glm(data$diabetes~data$pregnant+data$glucose+data$pressure+data$mass+data$pedigree,data=data,family=binomial())
+summary(model1) 
+
+#Predicting the model
+data$pred<-predict(model1,type='response')
+View(data)
+
+#Setting threshold
+data$preddiab<-ifelse(data$pred > 0.1,1,0)
+
+#Viewing the confusion matrix to check validity of prediction
+table(Actual=data$diabetes,Predicted=data$preddiab)
