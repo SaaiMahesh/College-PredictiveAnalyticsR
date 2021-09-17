8003ed59bf176151a00eb914230af47812c043c6
SaaiMahesh <82525561+SaaiMahesh@users.noreply.github.com>
2021-09-17 13:00:58 +0530

    Update Logistic Regression Model Prediction.R

--- Logistic Regression Model Prediction.R
+++ Logistic Regression Model Prediction.R
@@ -19,4 +19,53 @@ View(data) #To view the newly updated data, wherein pred column was added after 
 
 #Creating threshold to result for the pred column where it shows that if it is >0.1 it is 1 and <0.1 it is 0.
 data$purch<-ifelse(data$pred>0.1,1,0)
-table(FR=data$Florence,PR=data$purch) #Where we defined FR for actual values of florence and PR for predicted values
+table(Actual=data$Florence,Predicted=data$purch) #Gets us the confusion matrix for predicted and actual values. Where we defined Actual for actual values of florence and Predicted for predicted values.
+#Actual values of 0 shows that people did not buy the book but predicted shows 382 meaning that it is wrong prediction.(A0-P1 should be low, FALSE POSITIVE)
+#Actual values of 1 shows that people did buy the book but predicted shows 85 did not meaning that it is wrong prediction.. (A1-P0 should be lower values FALSE NEGATIVE)
+#Similarly, A0-P0 should be high (TRUE NEGATIVE), A1-P1 should be high (TRUE POSITIVE)
+
+
+#Hence, we create another model to get better confusion matrix
+model2<-glm(Florence~M+ArtBks,data=data,family=binomial())
+summary(model2) #AIC is lesser than model1, so we continue
+
+#Model prediction
+data$pred<-predict(model2,type='response')
+View(data)
+
+#Defining threshold
+data$purch<-ifelse(data$pred>0.1,1,0)
+table(Actual=data$Florence,Predicted=data$purch) #This shows that model 2 is best so far.
+
+
+#Building 3rd model
+model3<-glm(Florence~M+ArtBks+CookBks,data=data,family=binomial())
+summary(model3) #AIC is lesser than model2, so we continue
+
+#Model Prediction
+data$pred<-predict(model3,type='response')
+View(data)
+
+#Defining threshold
+data$purch<-ifelse(data$pred>0.1,1,0)
+table(Actual=data$Florence,Predicted=data$purch) #Showing more errors than previous model so we have to change the predictors for independent variables.
+
+
+#Building 4th model
+model4<-glm(Florence~M+ArtBks+CookBks+ChildBks,data=data,family=binomial())
+summary(model4)
+
+#Model prediction
+data$pred<-predict(model4,type='response')
+View(data)
+
+#Defining threshold
+data$purch<-ifelse(data$pred > 0.1,1,0)
+table(Actual=data$Florence,Predicted=data$purch) #Showing more errors than previous model so we have to change the predictors for independent variables.
+
+
+#Sensitivity = TP/FN+TP
+#Specificity = TN/TN+FP
+
+
+data<-PimaIndiansDiabetes
