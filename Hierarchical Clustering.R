f03a57e5869e9a8d583d089d02b480e8fa3d1049
SaaiMahesh <82525561+SaaiMahesh@users.noreply.github.com>
2021-10-01 12:51:37 +0530

    Add files via upload

---
+++ Hierarchical Clustering.R
@@ -0,0 +1,51 @@
+#Note: It is unsupervised because we are not splitting the data into train and test data.
+library(cluster.datasets)
+data(nutrients.meat.fish.fowl.1959)
+data<-nutrients.meat.fish.fowl.1959
+
+View(data)
+str(data)
+summary(data)
+
+#Cleaning data
+sum(is.na(data)) #No NAs present
+data<-data[,-1] #Removing categorical column
+
+#Scaling data
+datascaled<-scale(data) #Scaling data
+
+
+#Creating hierarchical clusters
+cluster1<-hclust(dist(datascaled),method='centroid') #Creating hierarchical clusters using centroid method - distance between the centroids of two clusters. 
+
+cluster2<-hclust(dist(datascaled),method='average') #Creating hierarchical clusters using average method -  distance between each pair of observations in each cluster are added up and divided by the number of pairs to get an average inter-cluster distance.
+
+cluster3<-hclust(dist(datascaled),method='complete') #Creating hierarchical clusters using complete method - is where distance is measured between the farthest pair of observations in two clusters.
+
+cluster4<-hclust(dist(datascaled),method='single') #Creating hierarchical clusters using single method - is the shortest distance between a pair of observations in two clusters.
+
+
+#Plotting the dendograms for the hierarchical clusters
+plot(cluster1,main='Hierachial Cluster - Centroid') #Additional arguments are labels,cex and hang.
+
+plot(cluster2,main='Hierachial Cluster - Average')
+
+plot(cluster3,main='Hierachial Cluster - Complete')
+
+plot(cluster4,main='Hierachial Cluster - Single')
+
+#Finding the number of optimal clusters
+library(NbClust)
+numclust<-NbClust(datascaled,distance='euclidean',min.nc=2,max.nc=6,method='centroid') #We put max as 6 since there are max of 6 variables
+#We get optimal number of clusters as 5
+
+#Plotting using the cut dendogram using the number of optimal clusters
+plot(cluster1,main='Hierachial Cluster - Centroid') 
+rect.hclust(cluster1,k=5)
+
+#Viewing the number of data values under each cut cluster
+cutcluster<-cutree(cluster1,k=5)
+table(cutcluster) #This is the optimal number of values for each cut cluster
+
+
+
