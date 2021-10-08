bde0b47e21bc5f67f12ac41c9db3dd6d53c218da
SaaiMahesh <82525561+SaaiMahesh@users.noreply.github.com>
2021-10-08 12:03:30 +0530

    Add files via upload

---
+++ K-Medoids.R
@@ -0,0 +1,38 @@
+#K-Medoids is a method wherein instead of mean being the center of the cluster, is one of the actual observations from the cluster.
+#Using the World Bank Data in this exercise
+
+install.package('WDI')
+require(WDI) 
+
+#To pull information on the indicators for all the countries in the list, some countries dont have data
+indicators<-c("BX.KLT.DINV.WD.GD.ZS","NY.GDP.DEFL.KD.ZG","NY.GDP.MKTP.CD","NY.GDP.MKTP.KD.ZG","NY.GDP.PCAP.CD","NY.GDP.PCAP.KD.ZG","TG.VAL.TOTL.GD.ZS")
+
+#Assigning indicators for countries that dont have data
+wbinform<-WDI(country="all",indicator=indicators,start=2011,end=2011,extra=TRUE)
+
+#To remove aggregated information
+wbinform<-wbinform[wbinform$region!="Aggregates",]
+
+#To remove countries where values are NA
+wbinform<-wbinform[which(rowSums(!is.na(wbinform[,indicators]))>0),]
+
+#To remove rows where ISO is missing
+wbinform<-wbinform[!is.na(wbinform$iso2c),]
+
+#Set rownames so as to know the country without using it for clustering
+rownames(wbinform)<-wbinform$iso2c
+
+#Refactorize region,income and lending to account
+wbinform$region<-factor(wbinform$region)
+wbinform$income<-factor(wbinform$income)
+wbinform$lending<-factor(wbinform$lending)
+
+#To find columns to keep
+keepcols<-which(!names(wbinform)%in%c("iso2c","country","year","capital","iso3c"))
+
+#Fit the clustering
+library(cluster)
+wpam<-pam(x=wbinform[,keepcols],k=12,keep.diss=TRUE,keep.data=TRUE)
+
+#To see the medoid observations
+wpam$medoids
